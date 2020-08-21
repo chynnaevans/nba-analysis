4ba3820deeaac54b1d09db14f932059eee09d4ec
Chynna Evans <chynna06@gmail.com>
2020-08-21 13:43:03 -0400

    highest FG% by player, by zone

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # nba-analysis
 I found out that I can get a bunch of NBA data so I got excited and am playing around with it :-)
+
+Notes:
++ Must have `Naked` installed for Python – `pip install Naked`
--- importShotsData_python.js
+++ importShotsData_python.js
@@ -27,23 +27,20 @@ for ( var i = 2; i < args.length; i ++ ){
 }
 
 if(name != ""){
-	console.log("Finding player: " + name);
 	req['PlayerID'] = NBA.findPlayer(name).playerId;
-} else {
-	console.log("Fetching all shots for all players");
 }
 
 if(season != ""){
-	console.log("Finding season: " + season);
 	req['Season'] = season;
-} else {
-	console.log("Finding all seasons");
 }
+
+var fileName = "./shotData/" + name.replace(/\s+/g, '') + season + "Shots" + Date.now() + '.json';
 
 // Make API call
 NBA.stats.shots(req).then((shot) => {
-fs.writeFile("./shotData/" + name.replace(/\s+/g, '') + season + "Shots" + Date.now() + '.json', JSON.stringify(shot), function(err) {
+fs.writeFile(fileName, JSON.stringify(shot), function(err) {
 	if (err) return console.log(err);
-	console.log("did the thing");
 })
 });
+
+console.log(fileName);
